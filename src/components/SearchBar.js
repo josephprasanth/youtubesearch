1f9c6b4cc1e3ca720d6569a5a31e8dae3fa8b0a6
josephprasanth <prasanthjoseph_77@yahoo.com>
2021-01-25 14:23:48 +0100

    Added in semantic ui link and some searchbar component for the search term

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {
+        term: ''
+    }
+
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value });
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.term);
+        // Section for call back from parent component -- The searchterm
+    };
+
     render() {
-        return <div>SearchBar</div>
+        return (
+            <div class="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form" action="">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
